dfc72186252703a316d9f163d284714bbf03ab11
krow7 <krow7@ya.ru>
2020-04-19 18:01:35 +0300

    changed to main build branch to master

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,7 +2,7 @@
 BRANCH=$1
 LOGIN=$2
 PASSWORD=$3
-if [[ "$BRANCH" == "test-build" ]]; then
+if [[ "$BRANCH" == "master" ]]; then
   echo "$PASSWORD" | docker login -u "$LOGIN" --password-stdin;
   docker push krow7/hlds:1.1;
 fi
