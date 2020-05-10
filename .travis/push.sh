f3c72c17d009dff5f999f3238fa95f1bb9186a77
alesana-san <35428099+alesana-san@users.noreply.github.com>
2020-05-10 14:11:38 +0300

    Feature fix travis (#24)

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-BRANCH=$1
-LOGIN=$2
-PASSWORD=$3
-if [[ "$BRANCH" == "master" ]]; then
-  echo "$PASSWORD" | docker login -u "$LOGIN" --password-stdin;
-  docker push krow7/hlds:1.1;
-fi
+LOGIN=$1
+PASSWORD=$2
+BRANCH=$3
+PR=$4
+if [[ "$BRANCH" == "master" && ! ${PR} ]]; then
+  echo "$PASSWORD" | docker login -u "$LOGIN" --password-stdin || exit 1
+  docker push krow7/hlds:test || exit 1
+fi || exit 1
