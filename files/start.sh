4141ec6bff4d5d0546d58eed2873ef755e57da1c
alesana-san <35428099+alesana-san@users.noreply.github.com>
2020-05-10 19:32:11 +0300

    Feature slim (#26)
    updated Dockerfile to compress all RUN commands and slightly changed build strategy
    refactored start.sh
    Added VERSION support
    Added direct download of dproto instead of having it here
    Added ca-certificates to avoid errors in server console log
    Added know issues block to README

--- files/start.sh
+++ files/start.sh
@@ -1,75 +1,77 @@
 #!/usr/bin/env bash
-mkdir -p $OPTS_PATH/config
-mkdir -p $OPTS_PATH/amxx
+OPTS=$1
+HLDS=$2
+mkdir -p ${OPTS}/config
+mkdir -p ${OPTS}/amxx
 
-if [ ! -f $OPTS_PATH/config/mapcycle.txt ]; then
-	mv $HLDS_PATH/cstrike/mapcycle.txt $OPTS_PATH/config
+if [[ ! -f ${OPTS}/config/mapcycle.txt ]]; then
+	mv ${HLDS}/cstrike/mapcycle.txt ${OPTS}/config
 fi
 
-if [ ! -f $OPTS_PATH/config/motd.txt ]; then
-	mv $HLDS_PATH/cstrike/motd.txt $OPTS_PATH/config
+if [[ ! -f ${OPTS}/config/motd.txt ]]; then
+	mv ${HLDS}/cstrike/motd.txt ${OPTS}/config
 fi
 
-if [ ! -f $OPTS_PATH/config/listenserver.cfg ]; then
-	mv $HLDS_PATH/cstrike/listenserver.cfg $OPTS_PATH/config
+if [[ ! -f ${OPTS}/config/listenserver.cfg ]]; then
+	mv ${HLDS}/cstrike/listenserver.cfg ${OPTS}/config
 fi
 
-if [ ! -f $OPTS_PATH/config/server.cfg ]; then
-	mv $HLDS_PATH/cstrike/server.cfg $OPTS_PATH/config
+if [[ ! -f ${OPTS}/config/server.cfg ]]; then
+	mv ${HLDS}/cstrike/server.cfg ${OPTS}/config
 fi
 
-if [ ! -f $OPTS_PATH/config/plugins.ini ]; then
-	mv $HLDS_PATH/cstrike/addons/metamod/plugins.ini $OPTS_PATH/config
+if [[ ! -f ${OPTS}/config/plugins.ini ]]; then
+	mv ${HLDS}/cstrike/addons/metamod/plugins.ini ${OPTS}/config
 fi
 
-if [ ! -f $OPTS_PATH/config/dproto.cfg ]; then
-	mv $HLDS_PATH/cstrike/dproto.cfg $OPTS_PATH/config
+if [[ ! -f ${OPTS}/config/dproto.cfg ]]; then
+	mv ${HLDS}/cstrike/dproto.cfg ${OPTS}/config
 fi
 
-if [ ! -d $OPTS_PATH/maps ]; then
-	mv $HLDS_PATH/cstrike/maps $OPTS_PATH/
+if [[ ! -d ${OPTS}/maps ]]; then
+	mv ${HLDS}/cstrike/maps ${OPTS}/
 fi
 
-if [ ! -d $OPTS_PATH/logs ]; then
-	mv $HLDS_PATH/cstrike/logs $OPTS_PATH/
+if [[ ! -d ${OPTS}/logs ]]; then
+	mv ${HLDS}/cstrike/logs ${OPTS}/
 fi
 
-if [ ! -d $OPTS_PATH/amxx/configs ]; then
-	mv $HLDS_PATH/cstrike/addons/amxmodx/configs $OPTS_PATH/amxx
+if [[ ! -d ${OPTS}/amxx/configs ]]; then
+	mv ${HLDS}/cstrike/addons/amxmodx/configs ${OPTS}/amxx
 fi
 
-if [ ! -d $OPTS_PATH/amxx/logs ]; then
-	mv $HLDS_PATH/cstrike/addons/amxmodx/logs $OPTS_PATH/amxx
+if [[ ! -d ${OPTS}/amxx/logs ]]; then
+	mv ${HLDS}/cstrike/addons/amxmodx/logs ${OPTS}/amxx
 fi
 
-if [ ! -d $OPTS_PATH/amxx/modules ]; then
-	mv $HLDS_PATH/cstrike/addons/amxmodx/modules $OPTS_PATH/amxx
+if [[ ! -d ${OPTS}/amxx/modules ]]; then
+	mv ${HLDS}/cstrike/addons/amxmodx/modules ${OPTS}/amxx
 fi
 
-if [ ! -d $OPTS_PATH/amxx/plugins ]; then
-	mv $HLDS_PATH/cstrike/addons/amxmodx/plugins $OPTS_PATH/amxx
+if [[ ! -d ${OPTS}/amxx/plugins ]]; then
+	mv ${HLDS}/cstrike/addons/amxmodx/plugins ${OPTS}/amxx
 fi
 
-rm -rf $HLDS_PATH/cstrike/addons/amxmodx/plugins
-rm -rf $HLDS_PATH/cstrike/addons/amxmodx/modules
-rm -rf $HLDS_PATH/cstrike/addons/amxmodx/logs
-rm -rf $HLDS_PATH/cstrike/addons/amxmodx/configs
-rm -rf $HLDS_PATH/cstrike/maps
-rm -rf $HLDS_PATH/cstrike/logs
+rm -rf ${HLDS}/cstrike/addons/amxmodx/plugins
+rm -rf ${HLDS}/cstrike/addons/amxmodx/modules
+rm -rf ${HLDS}/cstrike/addons/amxmodx/logs
+rm -rf ${HLDS}/cstrike/addons/amxmodx/configs
+rm -rf ${HLDS}/cstrike/maps
+rm -rf ${HLDS}/cstrike/logs
 
-ln -s $OPTS_PATH/amxx/plugins $HLDS_PATH/cstrike/addons/amxmodx/plugins
-ln -s $OPTS_PATH/amxx/modules $HLDS_PATH/cstrike/addons/amxmodx/modules
-ln -s $OPTS_PATH/amxx/logs $HLDS_PATH/cstrike/addons/amxmodx/logs
-ln -s $OPTS_PATH/amxx/configs $HLDS_PATH/cstrike/addons/amxmodx/configs
-ln -s $OPTS_PATH/maps $HLDS_PATH/cstrike/maps
-ln -s $OPTS_PATH/logs $HLDS_PATH/cstrike/logs
-ln -sf $OPTS_PATH/config/dproto.cfg $HLDS_PATH/cstrike/dproto.cfg
-ln -sf $OPTS_PATH/config/plugins.ini $HLDS_PATH/cstrike/addons/metamod/plugins.ini
-ln -sf $OPTS_PATH/config/server.cfg $HLDS_PATH/cstrike/server.cfg
-ln -sf $OPTS_PATH/config/listenserver.cfg $HLDS_PATH/cstrike/listenserver.cfg
-ln -sf $OPTS_PATH/config/motd.txt $HLDS_PATH/cstrike/motd.txt
-ln -sf $OPTS_PATH/config/mapcycle.txt $HLDS_PATH/cstrike/mapcycle.txt
+ln -s ${OPTS}/amxx/plugins ${HLDS}/cstrike/addons/amxmodx/plugins
+ln -s ${OPTS}/amxx/modules ${HLDS}/cstrike/addons/amxmodx/modules
+ln -s ${OPTS}/amxx/logs ${HLDS}/cstrike/addons/amxmodx/logs
+ln -s ${OPTS}/amxx/configs ${HLDS}/cstrike/addons/amxmodx/configs
+ln -s ${OPTS}/maps ${HLDS}/cstrike/maps
+ln -s ${OPTS}/logs ${HLDS}/cstrike/logs
+ln -sf ${OPTS}/config/dproto.cfg ${HLDS}/cstrike/dproto.cfg
+ln -sf ${OPTS}/config/plugins.ini ${HLDS}/cstrike/addons/metamod/plugins.ini
+ln -sf ${OPTS}/config/server.cfg ${HLDS}/cstrike/server.cfg
+ln -sf ${OPTS}/config/listenserver.cfg ${HLDS}/cstrike/listenserver.cfg
+ln -sf ${OPTS}/config/motd.txt ${HLDS}/cstrike/motd.txt
+ln -sf ${OPTS}/config/mapcycle.txt ${HLDS}/cstrike/mapcycle.txt
 
-cd $HLDS_PATH/
+cd ${HLDS}/
 ##./hlds_run -game cstrike +maxplayers 32 +map de_dust2 +hostname "Krow7hosT" -nosteam -nomaster -insecure -port 27015 -dev
 ./hlds_run -game cstrike +maxplayers 32 +map de_dust2 -port 27015 -dev +log on
