82ce1a7909bf2d2e0b6d5085f5b60c7f7be3ec36
krow7 <krow7@ya.ru>
2020-05-10 13:36:24 +0300

    test if docker login will fail while pushing image to dockerhub on bad password

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,7 +2,7 @@
 BRANCH=$1
 LOGIN=$2
 PASSWORD=$3
-if [[ "$BRANCH" == "master" ]]; then
-  echo "$PASSWORD" | docker login -u "$LOGIN" --password-stdin;
-  docker push krow7/hlds:1.1;
-fi
+if [[ "$BRANCH" == "feature-fix-travis" ]]; then
+  echo "$PASSWORD" | docker login -u "$LOGIN" --password-stdin
+  docker push krow7/hlds:1.1
+fi || exit 1
